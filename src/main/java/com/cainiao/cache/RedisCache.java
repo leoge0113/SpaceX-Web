66ec1067917de0b29dbd50a93ef5a7bab4b3ed6b
leoge0113 <361350493@qq.com>
2018-01-06 19:18:04 +0800

    redis operation

--- src/main/java/com/cainiao/cache/RedisCache.java
+++ src/main/java/com/cainiao/cache/RedisCache.java
@@ -2,11 +2,15 @@ package com.cainiao.cache;
 
 import com.cainiao.ProtoStuffSerializerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.swing.*;
+//import javax.swing.*;
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class RedisCache {
@@ -18,8 +22,92 @@ public class RedisCache {
     public <T> boolean putCache(String key, T obj) {
         final byte[] bKey = key.getBytes();
         final byte[] bValue = ProtoStuffSerializerUtil.serializer(obj);
-        boolean result = redisTemplate.execute((RedisCallback<Boolean>) (connection)->{
-            return connection.setNX(bKey,bValue);});//函数接口是个泛型接口
+        boolean result = redisTemplate.execute((RedisCallback<Boolean>) (connection) -> {
+            return connection.setNX(bKey, bValue);
+        });//函数接口是个泛型接口
         return result;
+    }
+
+    public <T> boolean putCacheWithExpireTime(String key, T obj, final long time) {
+        final byte[] bKey = key.getBytes();
+        final byte[] bValue = ProtoStuffSerializerUtil.serializer(obj);
+        boolean result = redisTemplate.execute((RedisCallback<Boolean>) (connection) -> {
+            connection.setEx(bKey, time, bValue);
+            return true;
+        });
+        return result;
+    }
+
+    public <T> boolean putListCache(String key, List<T> objList) {
+        final byte[] bkey = key.getBytes();
+        final byte[] bvalue = ProtoStuffSerializerUtil.listSerializer(objList);
+        boolean result = redisTemplate.execute((RedisCallback<Boolean>) (connection) -> {
+            return connection.setNX(bkey, bvalue);
+        });
+        return result;
+    }
+    public <T> boolean putListCacheWithExpireTime(String key, List<T> objList, final long expireTime) {
+        final byte[] bkey = key.getBytes();
+        final byte[] bvalue = ProtoStuffSerializerUtil.listSerializer(objList);
+        boolean result = redisTemplate.execute(new RedisCallback<Boolean>() {
+            @Override
+            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+                connection.setEx(bkey, expireTime, bvalue);
+                return true;
+            }
+        });
+        return result;
+    }
+
+    public <T> T getCache(final String key, Class<T> targetClass) {
+        byte[] result = redisTemplate.execute(new RedisCallback<byte[]>() {
+            @Override
+            public byte[] doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.get(key.getBytes());
+            }
+        });
+        if (result == null) {
+            return null;
+        }
+        return ProtoStuffSerializerUtil.deserializer(result, targetClass);
+    }
+
+    public <T> List<T> getListCache(final String key, Class<T> targetClass) {
+        byte[] result = redisTemplate.execute(new RedisCallback<byte[]>() {
+            @Override
+            public byte[] doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.get(key.getBytes());
+            }
+        });
+        if (result == null) {
+            return null;
+        }
+        return ProtoStuffSerializerUtil.deserializeList(result, targetClass);
+    }
+
+    /**
+     * 精确删除key
+     *
+     * @param key
+     */
+    public void deleteCache(String key) {
+        redisTemplate.delete(key);
+    }
+
+    /**
+     * 模糊删除key
+     *
+     * @param pattern
+     */
+    public void deleteCacheWithPattern(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
+    /**
+     * 清空所有缓存
+     */
+    public void clearCache() {
+        deleteCacheWithPattern(RedisCache.GOODSCACHE + "|*");
     }
 }
